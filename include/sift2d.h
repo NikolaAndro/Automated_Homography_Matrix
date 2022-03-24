ead9fe70b40a4dc6ed0eda901f2e36eb89b8172a
Andric <namdd@r12nadendla.managed.mst.edu>
2022-03-24 18:11:17 -0500

    reorganized

---
+++ include/sift2d.h
@@ -0,0 +1,17 @@
+/**
+ * @file sift2d.h
+ * @author Nikola Andric (namdd@umsystem.edu)
+ * @brief 
+ * @version 0.1
+ * @date 2022-03-24
+ * 
+ * @copyright Copyright (c) 2022
+ * 
+ */
+
+#ifndef SIFT2D_H
+#define SIFT2D_H
+
+
+
+#endif //SIFT2D_H
---
+++ src/sift2d.cpp
@@ -0,0 +1,11 @@
+/**
+ * @file sift2d.cpp
+ * @author Nikola Andric (namdd@umsystem.edu)
+ * @brief 
+ * @version 0.1
+ * @date 2022-03-24
+ * 
+ * @copyright Copyright (c) 2022
+ * 
+ */
+
